4852c812f8c4f0d2f766c7fd8641a4f6a9234924
GuhSntLean <guleandro.66@gmail.com>
2021-01-08 17:34:32 -0300

    Add Cards

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,11 +1,13 @@
 import './styles.css';
 
 import Stepheader from '../../component/Stepheader/Stepheader';
+import ProductsList from '../../component/ProductsList/ProductsList';
 
 function Orders(){
   return(
     <div className="orders-container">
       <Stepheader/>
+      <ProductsList/>
     </div>
   )  
 }
